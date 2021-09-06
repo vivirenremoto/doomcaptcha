3749840b7e5bfbf9da056415143b95e53c1b6552
man-of-eel <unconfigured@null.spigotmc.org>
2021-09-07 00:30:17 +0100

    Gun flash

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ if (captcha_label) {
     captcha_html = '<p>' + captcha_label + '<br>';
 }
 
-captcha_html += '<iframe id="doom_captcha" src="https://vivirenremoto.github.io/doomcaptcha/captcha.html?version=' + captcha_version + '&countdown=' + document.currentScript.getAttribute('countdown') + '&enemies=' + captcha_enemies + '" style="width:300px;height:150px;border:2px black solid;"></iframe>';
+captcha_html += '<iframe id="doom_captcha" src="captcha.html?version=' + captcha_version + '&countdown=' + document.currentScript.getAttribute('countdown') + '&enemies=' + captcha_enemies + '" style="width:300px;height:150px;border:2px black solid;"></iframe>';
 
 if (captcha_label) {
     captcha_html += '</p>';
@@ -19,14 +19,14 @@ if (captcha_label) {
 
 document.write(captcha_html);
 
-window.addEventListener('message', function(e) {
+window.addEventListener('message', function (e) {
     if (e.origin.indexOf('vivirenremoto.github.io') > -1) {
         captcha_done = true;
         document.getElementById('doom_captcha').style.borderColor = 'black';
     }
 }, false);
 
-document.getElementById('doom_captcha').closest('form').addEventListener('submit', function() {
+document.getElementById('doom_captcha').closest('form').addEventListener('submit', function () {
     if (!captcha_done) {
         document.getElementById('doom_captcha').style.borderColor = 'red';
         event.preventDefault();
